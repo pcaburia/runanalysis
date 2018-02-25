d267cd0e699e208307efe5f81458efce39347375
pcaburia <p.caburian@gmail.com>
2018-02-25 12:47:54 +1100

    files submitted

---
+++ run_analysis.R
@@ -0,0 +1,88 @@
+
+# PURPOSE: To prepare tidy data that can be used for later analysis.
+# PARAMS : parent_path is the path which contains the test and train folders.
+# RETURNS: a tidy data as a data frame.
+# NOTES  : The data files (subject, X, and y) for the train and test data set are merged into one data frame.
+#          Then a tidy data set is created from the merged data.
+run_analysis <- function(parent_path) {
+    
+    # Ensure folder contains both train and test sub paths and the features and activity_labels text files.
+    if (sum(list.files(parent_path) %in% c("test","train","features.txt","activity_labels.txt")) == 4) {
+        
+        activity_labels <- read.table(paste(sep = "/", parent_path, "activity_labels.txt"), header = FALSE)
+        names(activity_labels) <- c("activity_id", "activity")
+        
+        features <- read.table(paste(sep = "/", parent_path, "features.txt"), header = FALSE)
+        
+        # Create the column filter to select only the mean and sd columns.
+        features_mean_sd_colfilter <- grepl(pattern = "mean\\(|std\\(", x = features[,2])
+        
+        # Create the column names vector containing only the names of the mean and sd columns.
+        features_mean_sd_colnames <- features[features_mean_sd_colfilter, 2]
+        
+        # --- Process the measurements data sets ---
+        dataset_partitions = c("test", "train")
+        
+        # Process the data sets.
+        for (dataset_partition in dataset_partitions) {
+            
+            print("*****")
+            print(paste("Processing", dataset_partition, "data set."))
+            
+            # Create the dataset path.
+            dataset_path <- paste(sep = "/", parent_path, dataset_partition)
+            
+            print("Create data frame with subject.")
+            
+            # Create the temp data frame using the "subject" dataset and update the column name
+            dataset_item_path <- paste(sep = "", dataset_path, "/subject_", dataset_partition, ".txt")
+            dataset_df <- read.table(dataset_item_path, header = FALSE)
+            names(dataset_df) <- "subject"
+            
+            print(paste("CBind activity IDs for", length(dataset_df[,1]), dataset_partition, "records."))
+            
+            # cbind the activity IDs
+            dataset_item_path <- paste(sep = "", dataset_path, "/y_", dataset_partition, ".txt")
+            dataset_df <- cbind(dataset_df, read.table(dataset_item_path, header = FALSE))
+            names(dataset_df)[length(dataset_df)] <- "activity_id"
+            
+            print("Merge activity labels.")
+            
+            # Merge the activity labels
+            dataset_df <- merge(x = activity_labels, y = dataset_df, by.x = "activity_id", by.y = "activity_id")
+            
+            print(paste("Create temp data frame for", dataset_partition, " dataset."))
+            
+            # Obtain the data set for the type and put into a temp data frame
+            dataset_item_path <- paste(sep = "", dataset_path, "/X_", dataset_partition, ".txt")
+            temp_df <- read.table(dataset_item_path, header = FALSE)[, features_mean_sd_colfilter]
+            names(temp_df) <- features_mean_sd_colnames
+            
+            print("CBind temp to master data frame")
+            
+            # cbind the temp data frame to the master data frame.
+            dataset_df <- cbind(dataset_df, temp_df)
+            
+            if (dataset_partition == "test") {
+                master_df <- dataset_df
+            } else {
+                master_df <- rbind(master_df, dataset_df)
+            }
+        }
+        
+        print("*****")
+        print("Generating tidy data.")
+        
+        tidy_df <-  master_df %>%
+                        gather(measurement, measurement_value, -activity_id, -activity, -subject) %>% 
+                            group_by(subject, activity, measurement) %>%
+                                summarize(measurement_mean = mean(measurement_value))
+        
+        print("Done")
+        tidy_df
+    }
+}
+
+
+parent_path <- "./UCI HAR Dataset"
+tidy_df <- run_analysis(parent_path)
